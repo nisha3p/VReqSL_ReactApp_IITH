366bed064f32a515232a4f9e19ce7bea2a01cd6f
Nisha <nishajun2000@gmail.com>
2022-06-12 00:33:32 +0530

    Session Management

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,8 +1,12 @@
 const { json } = require('express');
 const express = require('express');
+const jwt = require('jsonwebtoken')
 const async = require('hbs/lib/async');
 const router = express.Router();
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
+
+
 //db
 const DB = process.env.DATABASE;
 mongoose.connect(DB, {
@@ -52,13 +56,18 @@ router.post('/register', async (req, res) => {
         if (userExist) {
             return res.status(422).json({ error: "Email already exist" });
         }
-        const user = new User({ email, password, cpassword })
-        const userRegister = await user.save()
-        if (userRegister) {
-            res.status(201).json({ message: "Registraion Successful" });
+        else if (password != cpassword) {
+            return res.status(422).json({ error: "Password does not match" });
         }
         else {
-            res.status(500).json({ error: "Registration failed" })
+            const user = new User({ email, password, cpassword })
+            const userRegister = await user.save()
+            if (userRegister) {
+                res.status(201).json({ message: "Registraion Successful" });
+            }
+            else {
+                res.status(500).json({ error: "Registration failed" })
+            }
         }
     } catch (err) {
         console.log(err);
@@ -66,4 +75,38 @@ router.post('/register', async (req, res) => {
 
 });
 
+//login route
+router.post('/signin', async (req, res) => {
+    try {
+        let token;
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(422).json({ error: "Please fill all the details" });
+        }
+        const userLogin = await User.findOne({ email: email });
+        if (userLogin) {
+            const isMatch = await bcrypt.compare(password, userLogin.password)
+            token = await userLogin.generateAuthToken();
+            console.log(token);
+
+            res.cookie("jwttoken", token, {
+                expires: new Date(Date.now() + 86400000),
+                httpOnly: true
+            });
+
+            if (!isMatch) {
+                res.status(400).json({ error: "Invalid Credentials" })
+            }
+            else {
+                res.status(200).json({ message: "Sign In Successful" })
+            }
+        }
+        else {
+            res.status(400).json({ error: "Invalid Credentials" })
+        }
+    } catch (err) {
+        console.log(err);
+    }
+
+})
 module.exports = router; 
--- server/userSchema.js
+++ server/userSchema.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken')
+const bycrypt = require('bcrypt')
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -11,9 +14,38 @@ const userSchema = new mongoose.Schema({
     cpassword: {
         type: String,
         required: true
-    }
+    },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true
+            }
+        }
+    ]
 
 
 })
+//hashing the pasasword//
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bycrypt.hash(this.password, 12)
+        this.cpassword = await bycrypt.hash(this.cpassword, 12)
+    }
+    next();
+});
+//generating token
+userSchema.methods.generateAuthToken = async function () {
+    try {
+        let token = jwt.sign({ _id: this._id }, process.env.SECRET);
+        this.tokens = this.tokens.concat({ token: token });
+        await this.save();
+        return token;
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 const User = mongoose.model('USER', userSchema)
 module.exports = User;
